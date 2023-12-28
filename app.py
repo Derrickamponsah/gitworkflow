0c5321a80defd000e3188d149865cc767d21556c
unknown <Derrick>
2023-12-28 10:08:05 +0000

    Completed Arithmetic Operations of All Function
    Added subFunction
    Added divFunction
    Added powFunction
    Added prodFunction

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-1+2
+def addFunction(x,y):
+    return x+y
+
+def subFunction(x,y):
+    return x-y
+
+def divFunction(x,y):
+    return x/y
+
+def powfunction(x,y):
+    return x**y
+
+def prodFunction(x,y):
+    return x*y
